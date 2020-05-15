e091bc30ec4b24b15369696747fccdf0de367a79
chhetrijanuka <janukachhetri15@gmail.com>
2020-05-15 17:25:26 +0530

    inline template example

--- ngApp/src/app/app.component.ts
+++ ngApp/src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1 class="text-center text-warning"> Bootstarp and jquery installed.</h1>
+  <p class="text-center text-danger">writing a html code in the component.ts file inside the component decoder </p>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
